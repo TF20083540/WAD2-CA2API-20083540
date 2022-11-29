498b2f77b606488d02e1aa45fb7ea6fa7705bec0
TF20083540 <20083540@mail.wit.ie>
2022-11-29 15:05:21 +0000

    Mongo lab Users Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
