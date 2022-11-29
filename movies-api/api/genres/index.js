fcf5a6dc0dab160cf4834e79986d4b5685d8550a
TF20083540 <20083540@mail.wit.ie>
2022-11-29 16:11:55 +0000

    Mongo lab exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { genres} from './genresData';
+import Genres from './genreModel';
 import uniqid from 'uniqid'
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
+
+
+// Get all users
+router.get('/', async (req, res) => {
+    const genres = await Genres.find();
+    res.status(200).json(genres);
 });
 
 
